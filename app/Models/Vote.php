69fbd603197e3ae21921af2ebd026d9dc58431b3
mahdyfo <mahdyfof@gmail.com>
2022-05-13 23:22:31 +0200

    create vote model and migration

---
+++ app/Models/Vote.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Vote extends Model
+{
+    use HasFactory;
+
+    public function comment(): HasOne
+    {
+        return $this->hasOne(Comment::class);
+    }
+}
---
+++ database/migrations/2022_05_13_194642_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('connection_id');
+            $table->unsignedBigInteger('user_id');
+            $table->tinyInteger('score');
+            $table->timestamps();
+
+            $table->foreign('connection_id')->references('id')->on('connections')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
